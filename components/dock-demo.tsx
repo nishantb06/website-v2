dead3705eff47a5b1a793176eab270e110b1dce9
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-11-01 17:21:24 +0530

    fixing the doc at the top

--- components/dock-demo.tsx
+++ components/dock-demo.tsx
@@ -95,65 +95,67 @@ const DATA = {
 
 export function DockDemo() {
   return (
-    <TooltipProvider>
-    <Dock direction="middle">
-        {DATA.navbar.map((item) => (
-        <DockIcon key={item.label}>
+    <div className="fixed top-0 left-0 right-0 z-50">
+        <TooltipProvider>
+        <Dock direction="middle">
+            {DATA.navbar.map((item) => (
+            <DockIcon key={item.label}>
+                <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link
+                    href={item.href}
+                    aria-label={item.label}
+                    className={cn(
+                        buttonVariants({ variant: "ghost", size: "icon" }),
+                        "size-12 rounded-full",
+                    )}
+                    >
+                    <item.icon className="size-4" />
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>{item.label}</p>
+                </TooltipContent>
+                </Tooltip>
+            </DockIcon>
+            ))}
+            <Separator orientation="vertical" className="h-full" />
+            {Object.entries(DATA.contact.social).map(([name, social]) => (
+            <DockIcon key={name}>
+                <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link
+                    href={social.url}
+                    aria-label={social.name}
+                    className={cn(
+                        buttonVariants({ variant: "ghost", size: "icon" }),
+                        "size-12 rounded-full",
+                    )}
+                    >
+                    <social.icon className="size-4" />
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>{name}</p>
+                </TooltipContent>
+                </Tooltip>
+            </DockIcon>
+            ))}
+            <Separator orientation="vertical" className="h-full py-2" />
+            <DockIcon>
             <Tooltip>
-            <TooltipTrigger asChild>
-                <Link
-                href={item.href}
-                aria-label={item.label}
-                className={cn(
-                    buttonVariants({ variant: "ghost", size: "icon" }),
-                    "size-12 rounded-full",
-                )}
-                >
-                <item.icon className="size-4" />
-                </Link>
-            </TooltipTrigger>
-            <TooltipContent>
-                <p>{item.label}</p>
-            </TooltipContent>
+                <TooltipTrigger asChild>
+                <div className="rounded-full">
+                    <ModeToggle />
+                </div>
+                </TooltipTrigger>
+                <TooltipContent>
+                <p>Theme</p>
+                </TooltipContent>
             </Tooltip>
-        </DockIcon>
-        ))}
-        <Separator orientation="vertical" className="h-full" />
-        {Object.entries(DATA.contact.social).map(([name, social]) => (
-        <DockIcon key={name}>
-            <Tooltip>
-            <TooltipTrigger asChild>
-                <Link
-                href={social.url}
-                aria-label={social.name}
-                className={cn(
-                    buttonVariants({ variant: "ghost", size: "icon" }),
-                    "size-12 rounded-full",
-                )}
-                >
-                <social.icon className="size-4" />
-                </Link>
-            </TooltipTrigger>
-            <TooltipContent>
-                <p>{name}</p>
-            </TooltipContent>
-            </Tooltip>
-        </DockIcon>
-        ))}
-        <Separator orientation="vertical" className="h-full py-2" />
-        <DockIcon>
-        <Tooltip>
-            <TooltipTrigger asChild>
-            <div className="rounded-full">
-                <ModeToggle />
-            </div>
-            </TooltipTrigger>
-            <TooltipContent>
-            <p>Theme</p>
-            </TooltipContent>
-        </Tooltip>
-        </DockIcon>
-    </Dock>
-    </TooltipProvider>
+            </DockIcon>
+        </Dock>
+        </TooltipProvider>
+    </div>
   );
 }
