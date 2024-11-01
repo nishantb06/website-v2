e81a01330786621bd6c33562e6861d84088a2235
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-11-01 11:44:50 +0530

    css for centering the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,14 @@
 import { BentoDemo } from "@/components/bento-demo";
 import { MagicCardDemo } from "@/components/magic-card-demo";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 
 export default function Home() {
   return (
-    <main className="min-h-screen dotted-background bg-background">
+    <main className="min-h-screen dotted-background bg-background max-w-5xl mx-auto">
       <div className="container mx-auto px-4 py-16">
       </div>
       <BentoDemo />
       <MagicCardDemo />
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-
     </main>
   );
 }
