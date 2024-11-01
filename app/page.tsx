2be9930995442f4e3297178fe9084cf77f0332a1
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-11-01 08:02:04 +0530

    bento , bento demo , button installation not working

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { BentoDemo } from "@/components/bento-demo";
+
 export default function Home() {
   return (
     <main className="min-h-screen dotted-background bg-background">
       <div className="container mx-auto px-4 py-16">
       </div>
+      <BentoDemo />
     </main>
   );
 }
