79e733a7b1d288e68def60c49707725479e436fc
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-11-01 16:23:00 +0530

    working dock v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "@/components/navbar";
+import { DockDemo } from "@/components/dock-demo";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar />
+        <DockDemo />
         {children}
       </body>
     </html>
