b495f717720d8b66b4bad57136aaadf5fbc2d20c
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-11-01 08:38:53 +0530

    avatar changes not working

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { BentoDemo } from "@/components/bento-demo";
 import { MagicCardDemo } from "@/components/magic-card-demo";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 
 export default function Home() {
   return (
@@ -8,6 +10,11 @@ export default function Home() {
       </div>
       <BentoDemo />
       <MagicCardDemo />
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
     </main>
   );
 }
