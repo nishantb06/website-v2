2eae2742a3f862794149f6b1e48cb85cc2113ec9
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-11-02 20:30:14 +0530

    feat: collapsible command box

--- components/search.tsx
+++ components/search.tsx
@@ -18,36 +18,43 @@ import {
   CommandList,
   CommandSeparator,
 } from "@/components/ui/command"
+import { CollapsibleContent, CollapsibleTrigger, Collapsible } from "@radix-ui/react-collapsible"
 
 export function CommandDemo() {
   return (
-    <Command className="rounded-lg border shadow-md md:min-w-[450px]">
-      <CommandInput placeholder="Type a command or search..." />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Blogs">
-          <CommandItem>
-            <Calendar />
-            <span>Jetson Nano</span>
-          </CommandItem>
-          <CommandItem>
-            <Smile />
-            <span>LLama</span>
-          </CommandItem>
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup heading="Articles">
-          <CommandItem>
-            <Shapes />
-            <span>Classes</span>
-          </CommandItem>
-          <CommandItem>
-            <BookOpen />
-            <span>Git tips</span>
-          </CommandItem>
-        </CommandGroup>
-      </CommandList>
-    </Command>
+    <Collapsible>
+        <Command className="rounded-lg border shadow-md md:min-w-[450px]">
+        <CollapsibleTrigger>
+            <CommandInput placeholder="Coming Soon..." />
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+            <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup heading="Blogs">
+                <CommandItem>
+                    <Calendar />
+                    <span>Jetson Nano</span>
+                </CommandItem>
+                <CommandItem>
+                    <Smile />
+                    <span>LLama</span>
+                </CommandItem>
+                </CommandGroup>
+                <CommandSeparator />
+                <CommandGroup heading="Articles">
+                <CommandItem>
+                    <Shapes />
+                    <span>Classes</span>
+                </CommandItem>
+                <CommandItem>
+                    <BookOpen />
+                    <span>Git tips</span>
+                </CommandItem>
+                </CommandGroup>
+            </CommandList>
+        </CollapsibleContent>
+        </Command>
+    </Collapsible>
   )
 }
 //
