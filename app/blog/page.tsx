f8474ada0768f5d0a513bbafe7a0c8bb0bc4ab49
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-11-02 19:01:55 +0530

    feat: search added

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
+
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div className="py-10 mx-auto max-w-2xl">
+      <div className="flex justify-center w-full max-w-2xl mx-auto py-20">
+        <div className="flex gap-2 w-3/4">
+          <Input className="h-10" placeholder="Coming soon..." />
+          <Button>
+            <SearchIcon className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
