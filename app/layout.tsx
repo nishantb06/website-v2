0af217fe4a5254a3a388afdae8e44a198a118875
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-11-23 11:08:23 +0530

    feat: improving dotted background

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="dotted-background bg-background">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Bento } from "@/components/bento";
 
 export default function Home() {
   return (
-    <main className="min-h-screen dotted-background bg-background max-w-5xl mx-auto">
+    <main className="min-h-screen max-w-5xl mx-auto">
       <div className="container mx-auto px-4 py-4">
       </div>
       <Bento />
