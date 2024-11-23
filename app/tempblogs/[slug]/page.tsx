af46407a559b3cd072def5c676961c8d5a02ebfe
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-11-23 13:47:25 +0530

    feat: simple rendering notion content

--- app/tempblogs/[slug]/page.tsx
+++ app/tempblogs/[slug]/page.tsx
@@ -1,11 +1,24 @@
-import { fetchBlogBlocks, fetchBlogBySlug } from "@/lib/notion";
+import { fetchBlogBlocks, fetchBlogBySlug, notion } from "@/lib/notion";
+import bookmarkPlugin from "@notion-render/bookmark-plugin";
+import { NotionRenderer } from "@notion-render/client";
+import hljsPlugin from "@notion-render/hljs-plugin";
 import { notFound } from "next/navigation";
 
-export default async function TempBlog({ params }: { params: { slug: string } }) {
+export default async function TempBlog({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const blog = await fetchBlogBySlug(params.slug);
   if (!blog) {
     notFound();
   }
-  const content = await fetchBlogBlocks(blog.id);
-  return <div>{JSON.stringify(content)}</div>;
+  const blocks = await fetchBlogBlocks(blog.id);
+
+  const renderer = new NotionRenderer({ client: notion });
+  renderer.use(hljsPlugin({}));
+  renderer.use(bookmarkPlugin(undefined));
+  const html = await renderer.render(...blocks);
+
+  return <div className="prose" dangerouslySetInnerHTML={{ __html: html }} />;
 }
