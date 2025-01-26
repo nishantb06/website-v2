0685803e2148ee900635bbb3f723e1208428506a
nbhansali06@gmail.com <nbhansali06@gmail.com>
2025-01-26 16:20:03 +0530

    black and white github calendar theme

--- components/github-calendar.tsx
+++ components/github-calendar.tsx
@@ -6,6 +6,10 @@ export function GithubCal() {
   const { theme } = useTheme();
 
   const colorScheme = theme === "dark" ? "dark" : "light";
+  const customTheme = {
+    light: ['#FFFFFF', '#C0C0C0', '#808080', '#404040', '#000000'],
+    dark: ['#27272a', '#3f3f46', '#52525b', '#71717a', '#a1a1aa'],
+  };
 
   return (
     <div className="h-full w-full flex items-center justify-center p-2">
@@ -15,6 +19,7 @@ export function GithubCal() {
           username="nishantb06"
           showWeekdayLabels={false}
           fontSize={16}
+          theme={customTheme}
         />
       </div>
     </div>
