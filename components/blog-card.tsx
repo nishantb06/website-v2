d2ba5d83920599279dca6b673ede9cd0c0ca6f71
nbhansali06@gmail.com <nbhansali06@gmail.com>
2025-01-26 16:50:12 +0530

    feat: slug and arrow icon working

--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -3,17 +3,20 @@
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import React from "react";
+import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
 
 interface BlogCardProps {
   title: string;
   subtitle: string;
   tags: string[];
+  slug: string;
   coverImage?: string;
 }
-export const BlogCard = ({ title, subtitle, tags, coverImage }: BlogCardProps) => {
+export const BlogCard = ({ title, subtitle, tags, coverImage, slug }: BlogCardProps) => {
   return (
-    <Card className="flex flex-col border-none shadow-md mb-4">
-      <CardHeader>
+    <Card className="flex flex-col border-none shadow-md mb-4 hover:-translate-y-1 group">
+      <CardHeader className="relative">
         {coverImage && (
             <div className="w-16 h-16 rounded-lg overflow-hidden shrink-0">
               <img 
@@ -25,6 +28,9 @@ export const BlogCard = ({ title, subtitle, tags, coverImage }: BlogCardProps) =
           )}
         <CardTitle>{title}</CardTitle>
         <CardDescription>{subtitle}</CardDescription>
+        <Link href={`/blog/${slug}`}>
+          <ArrowRightIcon className="w-5 h-5 absolute top-4 right-4 opacity-0 transition-all duration-300 group-hover:opacity-100 cursor-pointer" />
+        </Link>
       </CardHeader>
       <CardContent>
         <div>
